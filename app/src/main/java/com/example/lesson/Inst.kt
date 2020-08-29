363b68591734fdd3dcc2eb221c1be5a74b76cabb
DESKTOP-5OQ3NDI\MSI <georgedance2006@gmail.com>
2020-08-29 14:19:27 +0300

    no message

--- app/src/main/java/com/example/lesson/Inst.kt
+++ app/src/main/java/com/example/lesson/Inst.kt
@@ -1,7 +1,9 @@
 package com.example.lesson
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.lesson.Helper.Companion.gotoUrl
+import kotlinx.android.synthetic.main.activity_helpful_links.*
 import kotlinx.android.synthetic.main.activity_inst.*
 
 class Inst : BaseChildActivity() {
@@ -10,24 +12,16 @@ class Inst : BaseChildActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inst)
 
-        dancersUnderstand.setOnClickListener {
-            gotoUrl(it.context, getString(R.string.button_link_dancersunderstand))
+
+        val titles = resources.getStringArray(R.array.instagram_button_titles)
+        val urls = resources.getStringArray(R.array.instagram_button_urls)
+
+        val listAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, titles)
+        listOfInstagrams.adapter = listAdapter
+
+        listOfInstagrams.setOnItemClickListener { _, _, position, _ ->
+            gotoUrl(this@Inst, urls[position])
         }
 
-        fk_dancesport.setOnClickListener {
-            gotoUrl(it.context, getString(R.string.button_link_fk_dancesport_1))
-        }
-
-        QuickDanceSport.setOnClickListener {
-            gotoUrl(it.context, getString(R.string.button_link_QuickDancePhoto))
-        }
-
-        ballroomPhoto.setOnClickListener {
-            gotoUrl(it.context, getString(R.string.button_link_ballroomphoto))
-        }
-
-        ballroomPhoto.setOnClickListener {
-            gotoUrl(it.context, getString(R.string.button_link_ballroomphoto))
-        }
     }
 }
---
+++ app/src/main/java/com/example/lesson/NavigatorActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lesson
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NavigatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigator)
+    }
+}
