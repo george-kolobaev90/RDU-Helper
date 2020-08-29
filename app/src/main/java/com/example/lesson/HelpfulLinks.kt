9d0d9cec20fc9679fbab45b0fd4b5af8f6104fae
DESKTOP-5OQ3NDI\MSI <georgedance2006@gmail.com>
2020-08-29 13:43:59 +0300

    We added new functions to our app

--- app/src/main/java/com/example/lesson/HelpfulLinks.kt
+++ app/src/main/java/com/example/lesson/HelpfulLinks.kt
@@ -1,11 +1,23 @@
 package com.example.lesson
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.lesson.Helper.Companion.gotoUrl
+import kotlinx.android.synthetic.main.activity_helpful_links.*
 
 class HelpfulLinks : BaseChildActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_helpful_links)
+
+        val titles = resources.getStringArray(R.array.federations_button_titles)
+        val urls = resources.getStringArray(R.array.federations_button_urls)
+
+        val listAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, titles)
+        listOfFederations.adapter = listAdapter
+
+        listOfFederations.setOnItemClickListener { _, _, position, _ ->
+            gotoUrl(this@HelpfulLinks, urls[position])
+        }
     }
 }
