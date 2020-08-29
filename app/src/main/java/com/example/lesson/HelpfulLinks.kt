ba13f1e1e9ed7df8fd64832644aac3cca364570c
DESKTOP-5OQ3NDI\MSI <georgedance2006@gmail.com>
2020-08-29 10:49:45 +0300

    New buttons

---
+++ app/src/main/java/com/example/lesson/HelpfulLinks.kt
@@ -0,0 +1,11 @@
+package com.example.lesson
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HelpfulLinks : BaseChildActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_helpful_links)
+    }
+}
